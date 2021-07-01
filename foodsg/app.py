69a178b9e13efdb1d588baab948797aad1205251
lazyboy4943 <lazyboyzone@gmail.com>
2021-07-01 13:16:11 +0800

    Added a login feature

--- foodsg/app.py
+++ foodsg/app.py
@@ -1,10 +1,35 @@
-from flask import render_template, redirect, Flask, session, request
+from flask import render_template, redirect, Flask, session, request, url_for
 from flask_session import Session
 from tempfile import mkdtemp
+import json
+import os
+import sqlite3
 #from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 #from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import getConnection, executeWriteQuery, executeReadQuery, login_required
+
+
+from authlib.integrations.flask_client import OAuth
+
+
+app = Flask(__name__)
+app.secret_key = '5yTuleqqRRdRwvCf'
+
+
+oauth = OAuth(app)
+google = oauth.register(
+    name='google',
+    client_id = '400883400079-0mgfa8lv7qco8f9dj1elpg2huv70llcs.apps.googleusercontent.com',
+    client_secret = 'YLdpERED0IZeD80ZPSZ59qIh',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    client_kwargs={'scope': 'openid email profile'},
+)
 
 # configure application, use filesystem insted of cookies, make sure responses aren't cached
 app = Flask(__name__)
@@ -32,13 +57,57 @@ sellers = {
     8: {"name": "Faadil Ahmed", "cuisine": "Indian", "foodavailable": True, "vegetarian": True},
 }
 
+# whatever that was
+
+@app.route('/login')
+def login():
+    google = oauth.create_client('google')
+    redirect_uri = url_for('authorize', _external=True)
+    return google.authorize_redirect(redirect_uri)
+
+@app.route('/authorize')
+def authorize():
+    token = google.authorize_access_token()
+    resp = google.get('userinfo')
+    user_info = resp.json()
+    user = oauth.google.userinfo()
+    # do something with the token and profile
+    session['email'] = user_info['email']
+    return redirect('/')
+
 @app.route("/")
 def homepage():
-    return render_template('home.html')
+    email = dict(session).get('email', None)
+    if email:
+        return redirect("/signedin")
+    else:
+        return redirect("/signedout")
+
+@app.route('/signedin')
+def signedin():
+    email = dict(session).get('email', None)
+    return render_template('homein.html', email = email)
+
+@app.route("/signedout")
+def signedout():
+    return render_template('homeout.html')
+
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key) 
+    return redirect('/')
+
 
 @app.route("/home")
 def test():
     return redirect("/")
+
+@app.route('/sell')
+def sell():
+    return 'this is the sell page'
+
     
 @app.route("/about")
 def about():
@@ -89,4 +158,4 @@ def signuppage():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, host = '0.0.0.0')
+    app.run(ssl_context="adhoc")
